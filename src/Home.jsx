db1be5b616b1fe8675896a1db2278bbf05dc9a7d
Mubarak1A <adesinamubarak123@example.com>
2023-12-30 18:01:54 +0100

    add error catch condition to data fetching

--- src/Home.jsx
+++ src/Home.jsx
@@ -4,16 +4,26 @@ import BlogList from "./components/BlogList";
 const Home = () => {
     const [blogs, setBlogs] = useState(null);
     const [isPending, setIsPending] = useState(true);
+    const [error, setError] = useState(null)
 
     useEffect(() => {
         setTimeout(() => {
             fetch("http://localhost:8000/blogs")
             .then(res => {
+                if(!res.ok) {
+                    throw Error (`${res.status}, could not fetch the data resource`);
+                }
                 return res.json();
             })
             .then(data => {
                 console.log(data);
                 setBlogs(data)
+                setIsPending(false)
+                setError(null)
+            })
+            .catch(err => {
+                console.log(err.message)
+                setError(err.message)
                 setIsPending(false)
             })
         }, 1000)},
@@ -21,6 +31,7 @@ const Home = () => {
 
     return ( 
         <div className="home">
+            {error && <div className="error-message">{error}</div>}
             {isPending && <div className="loading">Loading...</div>}
             {blogs && <BlogList blogs={blogs} title={"All Blogs"}/>}
         </div>
