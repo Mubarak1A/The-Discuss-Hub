80cd3cdb3da18283738d433dbb38fb75faa6db1c
Mubarak1A <adesinamubarak123@example.com>
2023-12-30 13:02:05 +0100

    start the filling the home page data

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,7 +1,20 @@
+import { useState } from "react";
+
 const Home = () => {
+    const [blogs, setBlogs] = useState([
+        { title: "My New Website", body: "lorem ipsum...", author: "Mario", id: 1 },
+        { title: "Welcome Party", body: "lorem ipsum...", author: "Yoshi", id: 2 },
+        { title: "Web Dev Top Tips", body: "lorem ipsum...", author: "Mario", id: 3 }
+    ]);
+
     return ( 
         <div className="home">
-            <h2>Home Page</h2>
+            { blogs.map((blog) => (
+                <div className="blog-preview" key={blog.id}>
+                    <h2>{  blog.title }</h2>
+                    <p>Written by: <strong>{ blog.author }</strong></p>
+                </div>
+            ))}
         </div>
      );
 }
