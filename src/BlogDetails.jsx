4c6ba5dd6c87cf17e4290aa9f121e9084ffaec6d
Mubarak1A <adesinamubarak123@example.com>
2024-01-02 11:21:18 +0100

    use useFetch hook to fetch data from data base

--- src/BlogDetails.jsx
+++ src/BlogDetails.jsx
@@ -1,11 +1,21 @@
 import { useParams } from "react-router-dom";
+import useFetch from "./usefetch";
 
 const BlogDetails = () => {
     const { id } = useParams();
+    const { data: blog, error, isPending } = useFetch('http://localhost:8000/blogs/' + id)
 
     return ( 
-        <div className="bog-details">
-            <h2>Blog Details{ id }</h2>
+        <div className="blog-details">
+            { isPending && <div>Loading...</div> }
+            { error && <div>{ error }</div> }
+            { blog && (
+                <article>
+                    <h2>{ blog.title }</h2>
+                    <p>{ blog.author }</p>
+                    <p>{ blog.body }</p>
+                </article>
+            ) }
         </div>
      );
 }
