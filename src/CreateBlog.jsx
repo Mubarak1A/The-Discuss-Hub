95f3593270a5b9215a811a676a90b1f752bcf521
Mubarak1A <adesinamubarak123@example.com>
2024-01-02 11:46:39 +0100

    style the create blog component

--- src/CreateBlog.jsx
+++ src/CreateBlog.jsx
@@ -2,6 +2,23 @@ const CreateBlog = () => {
     return ( 
         <div className="create-blog">
             <h2>Add a new blog</h2>
+            <form>
+                <label>Blog Title:</label>
+                <input
+                    type="text"
+                    required
+                />
+                <label>Blog Body:</label>
+                <textarea
+                    required
+                />
+                <label>Blog Author:</label>
+                <select>
+                    <option value="mario">Mario</option>
+                    <option value="yoshi">Yoshi</option>
+                </select>
+                <button>Add Blog</button>
+            </form>
         </div>
      );
 }
