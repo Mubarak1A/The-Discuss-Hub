89435f52064645753bf502ea23c8ca94c281e438
Mubarak1A <adesinamubarak123@example.com>
2024-01-02 12:02:11 +0100

    prevent default form function when submit form

--- src/CreateBlog.jsx
+++ src/CreateBlog.jsx
@@ -1,22 +1,40 @@
+import { useState } from "react";
+
 const CreateBlog = () => {
+    const [ title, setTitle ] = useState('');
+    const [ body, setBody ] = useState('');
+    const [ author, setAuthor ] = useState('');
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const blog = { title, body, author }
+    }
+
     return ( 
         <div className="create-blog">
             <h2>Add a new blog</h2>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <label>Blog Title:</label>
                 <input
                     type="text"
                     required
+                    value={title}
+                    onChange={(e) => setTitle(e.target.value)}
                 />
                 <label>Blog Body:</label>
                 <textarea
+                    rows='5'
                     required
+                    value={body}
+                    onChange={(e) => setBody(e.target.value)}
                 />
                 <label>Blog Author:</label>
-                <select>
-                    <option value="mario">Mario</option>
-                    <option value="yoshi">Yoshi</option>
-                </select>
+                <input
+                    type="text"
+                    required
+                    value={author}
+                    onChange={(e) => setAuthor(e.target.value)}
+                />
                 <button>Add Blog</button>
             </form>
         </div>
