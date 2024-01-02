be57ba534ce55f156218aec6bbae6a98bb046a70
Mubarak1A <adesinamubarak123@example.com>
2024-01-02 10:16:24 +0100

    solve indentation issue in useFetch hook

--- src/usefetch.jsx
+++ src/usefetch.jsx
@@ -9,28 +9,29 @@ const useFetch = (url) => {
     useEffect(() => {
         setTimeout(() => {
             fetch(url, { signal: abortCont.signal })
-            .then(res => {
-                if(!res.ok) {
-                    throw Error (`${res.status}, could not fetch the data resource`);
-                }
-                return res.json();
-            })
-            .then(data => {
-                console.log(data);
-                setData(data)
-                setIsPending(false)
-                setError(null)
-            })
-            .catch(err => {
-                if (err.name === 'AbortError') {
-                    console.log("fetch aborted");
-                } else {
-                    setError(err.message)
+                .then(res => {
+                    if(!res.ok) {
+                        throw Error (`${res.status}, could not fetch the data resource`);
+                    }
+                    return res.json();
+                })
+                .then(data => {
+                    console.log(data);
+                    setData(data)
                     setIsPending(false)
-                }
-            })
-        }, 1000)},
-        return () => abortCont.abort();
+                    setError(null)
+                })
+                .catch(err => {
+                    if (err.name === 'AbortError') {
+                        console.log("fetch aborted");
+                    } else {
+                        setError(err.message)
+                        setIsPending(false)
+                    }
+                })
+        }, 1000);
+        return (() => abortCont.abort());
+    }, 
     [url])
 
     return ({ data, isPending, error });
