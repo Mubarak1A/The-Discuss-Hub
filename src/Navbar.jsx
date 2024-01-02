45cc74ca5e0026549865eb110bb2804067f022db
Mubarak1A <adesinamubarak123@example.com>
2024-01-02 10:17:26 +0100

    change page title logo

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 const Navbar = () => {
     return (
         <nav className="navBar">
-            <h1>The Social Memoir</h1>
+            <h1>Discuss Hub</h1>
             <div className="links">
                 <Link to="/">Home</Link>
                 <Link to="/create-blog">New Blog</Link>
